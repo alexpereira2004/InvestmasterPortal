e684984965784528170ff5a1c21dbeddcfe20bd5
alexpereira2017 <alexpereira2004@gmail.com>
2021-08-17 10:37:15 -0300

    Feature: Funcionalidade especifica chamada pelo controller

--- src/main/java/br/com/lunacom/portal/resource/v2/MovimentoCompraController.java
+++ src/main/java/br/com/lunacom/portal/resource/v2/MovimentoCompraController.java
@@ -6,10 +6,19 @@ import br.com.lunacom.portal.domain.request.MovimentoCompraRequest;
 import br.com.lunacom.portal.domain.response.MovimentoCompraResponse;
 import br.com.lunacom.portal.repository.GenericRepository;
 import br.com.lunacom.portal.resource.v2.specification.MovimentoCompraSpecification;
+import br.com.lunacom.portal.service.MovimentoCompraService;
 import br.com.lunacom.portal.util.DataUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
+@Validated
 @RestController
 @RequestMapping("/v2/movimento-compra")
 public class MovimentoCompraController extends
@@ -18,6 +27,9 @@ public class MovimentoCompraController extends
                           MovimentoCompraResponse,
                           MovimentoCompraSpecification> {
 
+    @Autowired
+    private MovimentoCompraService service;
+
     public MovimentoCompraController(
             GenericRepository<MovimentoCompra> repository,
             DataUtil dataUtil,
@@ -25,5 +37,11 @@ public class MovimentoCompraController extends
             Converter<MovimentoCompraResponse, MovimentoCompra> responseConverter,
             MovimentoCompraSpecification specification) {
         super(repository, dataUtil, requestConverter, responseConverter, specification);
+    }
+
+    @PostMapping(value = "/importa-texto-csv")
+    public ResponseEntity<Void> createFromCsvText(@RequestBody @Valid String request) {
+        service.salvarCsv(request);
+        return ResponseEntity.ok().build();
     }
 }
