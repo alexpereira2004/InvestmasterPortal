58e6031b28c9b5e23d139439a741e01204ddb092
alexpereira2017 <alexpereira2004@gmail.com>
2021-12-13 11:56:21 -0300

    Feature: Novo layout do extrato no site da Clear

--- src/main/java/br/com/lunacom/portal/service/DividendoService.java
+++ src/main/java/br/com/lunacom/portal/service/DividendoService.java
@@ -20,21 +20,22 @@ public class DividendoService {
     private final DividendoRepository repository;
     private final AtivoService ativoService;
     private Set<Ativo> ativoSet = new HashSet<>();
+    public static final String REGEX = "<div class=\"table-content__item pointer\" role=\"button\" tabindex=\"0\">.+<soma-caption class=\"date soma-caption hydrated\">(.*)<\\/soma-caption>(?:.*\\s\\n){4}.*<soma-caption class=\"value soma-caption hydrated\">R\\$&nbsp;([\\.|\\d{1,3}]+,\\d{2}).*(CRÉDITO FRAÇÕES|JUROS S\\/CAPITAL|DIVIDENDOS|RENDIMENTO|\\* PROV \\* RENDIMENTO) (\\d*)(?:\\s*PAPEL\\s|\\s*|)(\\w*)";
 
     public void salvarHtml(String request) {
         List<Dividendo> dividendoList = new ArrayList<>();
-        String regex = "<div class=\\\"cont-date settlement\\\">(.*)<\\/div>\\r\\n\\s*<div class=\\\"cont-description\\\">(JUROS S\\/CAPITAL|DIVIDENDOS|RENDIMENTO|\\* PROV \\* RENDIMENTO) (\\d*)(?:\\s*|\\s*\\w*\\s)(\\w*)\\s*<\\/div>\\r\\n\\s*<div class=\\\"cont-value\\\">\\r\\n\\s*<span>(.*)<\\/span>";
-        Pattern pattern = Pattern.compile(regex);
+        Pattern pattern = Pattern.compile(REGEX);
         Matcher matcher = pattern.matcher(request);
         while (matcher.find()) {
-            final Integer quantidade = Integer.valueOf(matcher.group(3));
-            final Double valorTotal = StringParser.toDouble(matcher.group(5));
+            final Integer quantidade = matcher.group(4).isEmpty() ? 1 : Integer.valueOf(matcher.group(4));
+            final Double valorTotal = StringParser.toDouble(matcher.group(2));
+            final Double dividendoCalculado = calcularDividendo(valorTotal, quantidade);
             final Dividendo dividendo = Dividendo.builder()
-                    .dividendo(valorTotal / quantidade)
+                    .dividendo(dividendoCalculado)
                     .dataRecebimento(dataUtil.dataBrParaLocalDate(matcher.group(1)))
-                    .tipo(matcher.group(2))
+                    .tipo(matcher.group(3))
                     .quantidade(quantidade)
-                    .ativo(getAtivo(matcher.group(4)))
+                    .ativo(getAtivo(matcher.group(5)))
                     .valorTotal(valorTotal)
                     .build();
             dividendoList.add(dividendo);
@@ -42,6 +43,10 @@ public class DividendoService {
         removerDividendosExistentes(dividendoList);
 
         repository.saveAll(dividendoList);
+    }
+
+    private Double calcularDividendo(Double valorTotal, Integer quantidade) {
+        return valorTotal / quantidade;
     }
 
     private void removerDividendosExistentes(List<Dividendo> dividendoList) {
