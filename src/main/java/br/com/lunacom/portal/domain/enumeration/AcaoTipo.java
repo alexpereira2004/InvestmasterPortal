9f77500faa1176a0bd694289d329625aa06862fe
alexpereira2014 <alexpereira2004@gmail.com>
2024-06-02 23:30:51 -0300

    Novos Enums

---
+++ src/main/java/br/com/lunacom/portal/domain/enumeration/AcaoTipo.java
@@ -0,0 +1,25 @@
+package br.com.lunacom.portal.domain.enumeration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.EnumSet;
+
+@AllArgsConstructor
+@Getter
+public enum AcaoTipo {
+    ACAO("A", "Ação"),
+    BDR("B", "BDR"),
+    FII("F", "Fundo Imobiliário");
+
+    String codigo;
+    String Descricao;
+
+    public static AcaoTipo fromCodigo(String value) {
+        return EnumSet.allOf(AcaoTipo.class)
+                .stream()
+                .filter(it -> it.getCodigo().equals(value))
+                .findFirst()
+                .orElse(ACAO);
+    }
+}
---
+++ src/main/java/br/com/lunacom/portal/domain/enumeration/Boleano.java
@@ -0,0 +1,24 @@
+package br.com.lunacom.portal.domain.enumeration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.EnumSet;
+
+@AllArgsConstructor
+@Getter
+public enum Boleano {
+    SIM("S", "Sim"),
+    NAO("N","Não");
+
+    String codigo;
+    String Descricao;
+
+    public static Boleano fromCodigo(String value) {
+        return EnumSet.allOf(Boleano.class)
+                .stream()
+                .filter(it -> it.getCodigo().equals(value))
+                .findFirst()
+                .orElse(SIM);
+    }
+}
