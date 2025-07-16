9c56e4509134bbf40c476752610f8e4a819fc1c8
alexpereira2014 <alexpereira2004@gmail.com>
2025-07-16 00:13:35 -0300

    #48 Status no enum

--- src/main/java/br/com/lunacom/portal/domain/AgendamentoConfig.java
+++ src/main/java/br/com/lunacom/portal/domain/AgendamentoConfig.java
@@ -1,10 +1,13 @@
 package br.com.lunacom.portal.domain;
 
+import br.com.lunacom.portal.converter.attribute.StatusConverter;
+import br.com.lunacom.portal.domain.enumeration.Status;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -18,8 +21,8 @@ import javax.persistence.Table;
 public class AgendamentoConfig {
     @Id
     private Integer id;
-
     private String nome;
-
     private String cron;
+    @Convert(converter = StatusConverter.class)
+    private Status status;
 }
