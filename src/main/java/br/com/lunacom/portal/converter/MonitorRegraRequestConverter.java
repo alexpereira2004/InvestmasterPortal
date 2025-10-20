6be05d67bba726cf3631c8adde3bc9aa0f9f597a
alexpereira2014 <alexpereira2004@gmail.com>
2025-10-19 22:36:17 -0300

    CRUD v2: MonitorRegra

--- src/main/java/br/com/lunacom/portal/converter/MonitorRegraRequestConverter.java
+++ src/main/java/br/com/lunacom/portal/converter/MonitorRegraRequestConverter.java
@@ -1,20 +1,29 @@
 package br.com.lunacom.portal.converter;
 
+import br.com.lunacom.portal.domain.Ativo;
 import br.com.lunacom.portal.domain.entity.monitor.MonitorRegra;
 import br.com.lunacom.portal.domain.request.MonitorRegraRequest;
+import br.com.lunacom.portal.service.AtivoService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityNotFoundException;
+
+@AllArgsConstructor
 @Component
 public class MonitorRegraRequestConverter extends GenericConverter<MonitorRegraRequest, MonitorRegra> {
 
+    AtivoService ativoService;
 
     @Override
     public MonitorRegra encode(MonitorRegraRequest input) {
+        final Ativo ativo = ativoService
+                .pesquisarPorCodigo(input.getAtivoCodigo())
+                .orElseThrow(() -> new EntityNotFoundException("Ativo informado não existe"));
         return MonitorRegra.builder()
-                .id(input.getId())
-//                .ativoId(input.getAtivoId())
-//                .prioridade(input.getPrioridade())
-                .status(input.getStatus())
+                .ativoId(ativo.getId())
+                .prioridade(input.getPrioridade())
+                .status(input.getStatus().getCodigo())
                 .build();
     }
 
--- src/main/java/br/com/lunacom/portal/domain/request/MonitorRegraRequest.java
+++ src/main/java/br/com/lunacom/portal/domain/request/MonitorRegraRequest.java
@@ -1,10 +1,13 @@
 package br.com.lunacom.portal.domain.request;
 
+import br.com.lunacom.portal.domain.enumeration.Status;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,5 +16,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class MonitorRegraRequest extends GenericRequest {
     private Integer id;
-    private String status;
+    private Status status = Status.ATIVO;
+    @NotBlank(message = "Informe Código do Ativo (ativoVenda)")
+    private String ativoCodigo;
+    private Integer prioridade;
 }
