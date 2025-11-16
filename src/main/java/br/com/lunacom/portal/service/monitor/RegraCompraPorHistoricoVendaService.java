e53b3bc685f2386b1edceba7d63838f0550f66fc
alexpereira2014 <alexpereira2004@gmail.com>
2025-11-15 23:40:48 -0300

    #37 Ao criar uma nova regra ela devera estar ativa

--- src/main/java/br/com/lunacom/portal/service/monitor/RegraCompraPorHistoricoVendaService.java
+++ src/main/java/br/com/lunacom/portal/service/monitor/RegraCompraPorHistoricoVendaService.java
@@ -1,6 +1,7 @@
 package br.com.lunacom.portal.service.monitor;
 
 import br.com.lunacom.portal.domain.entity.monitor.RegraCompraPorHistoricoVenda;
+import br.com.lunacom.portal.domain.enumeration.Status;
 import br.com.lunacom.portal.repository.monitor.RegraCompraPorHistoricoVendaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,7 @@ public class RegraCompraPorHistoricoVendaService {
     private final RegraCompraPorHistoricoVendaRepository repository;
 
     public RegraCompraPorHistoricoVenda salvar(RegraCompraPorHistoricoVenda entity) {
+        entity.setStatus(Status.ATIVO);
         return repository.save(entity);
     }
 }
