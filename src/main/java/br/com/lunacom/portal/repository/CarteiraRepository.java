1aa12d9d3e94b619def33bdf28d706648bd7f36a
alexpereira2014 <alexpereira2004@gmail.com>
2025-11-25 22:32:39 -0300

    #19 Melhoria para fazer um unico delete

--- src/main/java/br/com/lunacom/portal/repository/CarteiraRepository.java
+++ src/main/java/br/com/lunacom/portal/repository/CarteiraRepository.java
@@ -2,12 +2,18 @@ package br.com.lunacom.portal.repository;
 
 import br.com.lunacom.comum.domain.Carteira;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CarteiraRepository extends JpaRepository<Carteira, Integer> {
 
     Optional<Carteira> findByAtivoCodigo(String nome);
 
-    void deleteByAtivoCodigo(String codigo);
+    @Modifying
+    @Query("DELETE FROM Carteira c WHERE c.ativo.codigo IN :codigos")
+    void deleteByAtivoCodigoIn(@Param("codigos") List<String> codigosAtivo);
 }
