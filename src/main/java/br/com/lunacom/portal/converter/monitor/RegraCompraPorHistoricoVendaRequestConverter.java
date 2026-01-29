0100542abe6b4c4c49ef167efec93be6d9dfc00e
alexpereira2014 <alexpereira2004@gmail.com>
2026-01-29 00:07:42 -0300

    Parametro nao esta funcionando - verificar

--- src/main/java/br/com/lunacom/portal/converter/monitor/RegraCompraPorHistoricoVendaRequestConverter.java
+++ src/main/java/br/com/lunacom/portal/converter/monitor/RegraCompraPorHistoricoVendaRequestConverter.java
@@ -26,7 +26,7 @@ public class RegraCompraPorHistoricoVendaRequestConverter implements OneWayConve
                 .buscarPorMonitorId(input.getMonitorId());
 
         final RegraCompraPorHistoricoVenda build = RegraCompraPorHistoricoVenda.builder()
-                .periodo(input.getPeriodo().getCodigo())
+//                .periodo(input.getPeriodo().getCodigo())
                 .excluirPrejuizos(input.getExcluirPrejuizos().getCodigo())
                 .monitor(monitor)
                 .build();
