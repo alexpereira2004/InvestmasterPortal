3ad4d248875bfaf37290a7b9c7e393db15268022
alexpereira2014 <alexpereira2004@gmail.com>
2026-01-31 22:42:22 -0300

    #60 Converte o objeto para response e ja preenche o valor da cotacao atual

--- src/main/java/br/com/lunacom/portal/service/monitor/RegraCompraPorHistoricoVendaService.java
+++ src/main/java/br/com/lunacom/portal/service/monitor/RegraCompraPorHistoricoVendaService.java
@@ -1,21 +1,30 @@
 package br.com.lunacom.portal.service.monitor;
 
+import br.com.lunacom.comum.domain.dto.CotacaoAgoraDto;
 import br.com.lunacom.comum.domain.entity.monitor.RegraCompraPorHistoricoVenda;
 import br.com.lunacom.comum.domain.enumeration.Status;
 import br.com.lunacom.comum.domain.enumeration.TipoMovimento;
+import br.com.lunacom.portal.converter.monitor.RegraCompraPorHistoricoVendaResponseConverter;
 import br.com.lunacom.portal.domain.request.MovimentoCompraRequest;
+import br.com.lunacom.portal.domain.response.RegraCompraPorHistoricoVendaResponse;
 import br.com.lunacom.portal.repository.monitor.RegraCompraPorHistoricoVendaRepository;
+import br.com.lunacom.portal.service.CotacaoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class RegraCompraPorHistoricoVendaService {
     private final RegraCompraPorHistoricoVendaRepository repository;
+    private final CotacaoService cotacaoService;
+    private final RegraCompraPorHistoricoVendaResponseConverter responseConverter;
 
     public RegraCompraPorHistoricoVenda salvar(RegraCompraPorHistoricoVenda entity) {
         repository.inativarRegrasPorMonitor(entity.getMonitor().getId());
@@ -24,8 +33,21 @@ public class RegraCompraPorHistoricoVendaService {
         return repository.save(entity);
     }
 
-    public Page<RegraCompraPorHistoricoVenda> pesquisarComPaginacao(MovimentoCompraRequest request, Pageable pageable) {
+    public Page<RegraCompraPorHistoricoVendaResponse> pesquisarComPaginacao(MovimentoCompraRequest request, Pageable pageable) {
+        final List<CotacaoAgoraDto> cotacaoAgoraDtos = cotacaoService
+                .pesquisarCotacaoAgora();
+        final Page<RegraCompraPorHistoricoVenda> all = repository
+                .findAll(pageable);
+        final Page<RegraCompraPorHistoricoVendaResponse> response = all.map(e -> responseConverter.encode(e));
+        response.forEach(regra -> {
+            final BigDecimal cotacaoAtual = cotacaoAgoraDtos.stream()
+                    .filter(i -> i.getCodigo().equals(regra.getNome()))
+                    .findFirst()
+                    .orElse(new CotacaoAgoraDto())
+                    .getCotacaoAtual();
+            regra.setCotacaoAtual(String.valueOf(cotacaoAtual));
+        });
 
-        return repository.findAll(pageable);
+        return response;
     }
 }
