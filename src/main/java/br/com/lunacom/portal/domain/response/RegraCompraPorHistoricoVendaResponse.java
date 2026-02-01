2dad95e1f297c106ffff4a56c1bd87d38c195e6e
alexpereira2014 <alexpereira2004@gmail.com>
2026-01-31 23:12:04 -0300

    #60 Comentarios desnecessarios

--- src/main/java/br/com/lunacom/portal/domain/response/RegraCompraPorHistoricoVendaResponse.java
+++ src/main/java/br/com/lunacom/portal/domain/response/RegraCompraPorHistoricoVendaResponse.java
@@ -10,18 +10,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class RegraCompraPorHistoricoVendaResponse {
-    private String id; //: 15,
-    private String nome; //: 'ABCB4',
-    private String idMovimentoVenda; //: 122,
-    private String tipo; //: TipoMovimento.ENTRADA, // Representado por 'E' no resultset
-    private String status; //: Status.ATIVO,        // Representado por 'A'
-    private String periodo; //: PeriodoVenda.ULTIMOS_12_MESES, // 'D' (pelo mapeamento Java) ou 'E' conforme o dado bruto
-    private String excluirPrejuizos; //: 'N',
-    private String recomendacao; //: 'N',
-    private String recomendacaoEscala; //: 0,
-    private String analise; //: 'O preço atual considerado para o calculo foi de R$ 23.65. O valor da aquisição na única venda foi de R$ 19.07. A relação entre os preços está 24.02% mais cara',
-    private String observacao; //: 'O preço atual considerado para o calculo foi de R$ 23.65. O valor da aquisição na única venda foi de R$ 19.07. A relação entre os preços está 24.02% mais cara',
-    private String idMonitor; //: 12,
-    private String cotacaoAlvo; //: 12.54,
-    private String cotacaoAtual; //: 13.60
+    private String id;
+    private String nome;
+    private String idMovimentoVenda;
+    private String tipo;
+    private String status;
+    private String periodo;
+    private String excluirPrejuizos;
+    private String recomendacao;
+    private String recomendacaoEscala;
+    private String analise;
+    private String observacao;
+    private String idMonitor;
+    private String cotacaoAlvo;
+    private String cotacaoAtual;
 }
